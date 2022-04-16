d6832e8f3eb9bcc50a39d4d923b1cb36710920b3
Hai Tran <harefx.dev@gmail.com>
2022-04-17 03:43:47 +0700

    add health check and first rspec test

---
+++ spec/requests/health_check_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'HealthChecks', type: :request do
+  describe 'GET /health_check' do
+    it 'returns http success' do
+      get '/health_check'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
